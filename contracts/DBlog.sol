1edad988f6a020cf02f00dd651982f5ff0f159bd
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-01 21:40:00 +0530

    comments explaining the transactions in contract

--- contracts/DBlog.sol
+++ contracts/DBlog.sol
@@ -27,11 +27,16 @@ contract DBlog{
 
     mapping(uint => address) blogOwnersMap;
 
+    //0.0001 ether for someone else reading - from contract to the blog Owner
+    //0.01 ether to create a blog - from blog Creator to contract
+    //sale price should be greater than 0.001 ethers
+    //contract owner will get 0.001 ether everytime a blog is sold
+    //the blog seller will get (salePrice - 0.001 ether)ether on selling a blog
+    //the blog buyer will pay the salePrice amount and from that the seller and contractOwner will get their share
+
     function readBlog(uint blogId) public {
         if(msg.sender != blogOwnersMap[blogId]){
-            payable(blogOwnersMap[blogId]).transfer(1 ether);
+            payable(blogOwnersMap[blogId]).transfer(0.0001 ether);
         }
     }
-
-
 }
