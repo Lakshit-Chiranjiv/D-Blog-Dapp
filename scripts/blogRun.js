33d17268db2733a8f386c30d6f576f90b3dc336d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-02 09:18:49 +0530

    added comments

--- scripts/blogRun.js
+++ scripts/blogRun.js
@@ -2,6 +2,8 @@ const { ethers } = require("hardhat");
 
 async function main() {
     try {
+
+        //deploying and contract address
         const [owner,user1,user2] = await ethers.getSigners();  
         const DBlog = await ethers.getContractFactory("DBlog");
         const dblogContract = await DBlog.deploy();
@@ -9,19 +11,24 @@ async function main() {
       
         console.log("DBlog contract deployed to:", dblogContract.address);
       
+        //contract name
         let contractName = await dblogContract.getContractName();
         console.log(contractName);
       
+        //create blog
         let txn = await dblogContract.createBlog('Some blog title','Blog body is lorem ipsum',4,true,{ value: ethers.utils.parseEther("0.01") });
         await txn.wait();
         console.log("A new blog created");
       
+        //get A Blog
         let blogDetails = await dblogContract.getABlog(0);
         console.log(blogDetails);
-      
+       
+        //checking blog owner
         console.log(blogDetails.blogOwner)
         console.log(blogDetails.blogOwner == owner.address)
       
+        //buy blog
         txn = await dblogContract.connect(user1).buyBlog(0,{ value: ethers.utils.parseEther("0.004") })
         await txn.wait()
         console.log(`User 1: ${user1.address} bought blog with blog id 0 from the owner: ${owner.address} at 0.004 ethers`);
@@ -29,9 +36,11 @@ async function main() {
         blogDetails = await dblogContract.getABlog(0);
         console.log(blogDetails);
       
+        //checking blog owner
         console.log(blogDetails.blogOwner)
         console.log(blogDetails.blogOwner == user1.address)
       
+        //removing blog from sale
         txn = await dblogContract.connect(user1).removeBlogFromSale(0);
         await txn.wait();
       
@@ -39,6 +48,7 @@ async function main() {
         console.log(blogDetails);
         console.log("user 1 is blog 0 owner : "+blogDetails.blogOwner == user1.address)
       
+        //trying to buy blog after it has been put off sale
         txn = await dblogContract.connect(user2).buyBlog(0,{ value: ethers.utils.parseEther("0.004") })
         await txn.wait()
       
