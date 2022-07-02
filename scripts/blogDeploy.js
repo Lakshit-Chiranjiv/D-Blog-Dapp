39847707b6b2c93822099aeec10c825414518c4d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-02 09:28:27 +0530

    deploy script to deplooy over some network

---
+++ scripts/blogDeploy.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    try {
+
+        //deploying and contract address
+        const [owner] = await ethers.getSigners();  
+        let ownerBalance = await owner.getBalance();
+        const DBlog = await ethers.getContractFactory("DBlog");
+        console.log("owner balance before deploying :",ownerBalance.toString());
+        const dblogContract = await DBlog.deploy();
+        // const dblogContract = await DBlog.deploy({ value: ethers.utils.parseEther("5.0") });
+        await dblogContract.deployed();
+        ownerBalance = await owner.getBalance();
+        console.log("owner balance after deploying :",ownerBalance.toString());
+      
+        console.log("DBlog contract deployed on localhost to address:", dblogContract.address);
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
