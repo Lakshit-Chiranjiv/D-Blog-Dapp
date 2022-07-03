791497cdae481e9dfb6030cefb01f872933528b1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 20:37:31 +0530

    modified button component for extra classes

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({btnText,txtSize}) => {
+const Button = ({btnText,txtSize,extraClasses}) => {
   return (
-    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 hover:bg-black hover:shadow-none hover:text-white transition-all text-${txtSize}`}>
+    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 hover:bg-black hover:shadow-none hover:text-white transition-all text-${txtSize} ${extraClasses}`}>
         {btnText}
     </button>
   )
