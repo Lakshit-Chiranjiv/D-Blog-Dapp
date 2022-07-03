dee9560c63af9fed7be6dc05ec076ec8c23f36e5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 23:39:59 +0530

    page state var and conditional page rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,39 @@
 
+import { useState } from 'react'
 import './App.css'
 import About from './components/About'
 import BlogDetailPage from './components/BlogDetailPage'
 import BlogList from './components/BlogList'
-import Button from './components/Button'
 import CreatePage from './components/CreatePage'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
 
 function App() {
+  const [page,setPage] = useState('create')
 
   return (
     <div className="App bg-gradient-to-tl from-slate-500 via-gray-700 to-neutral-400 mb-0">
-      <Nav/>
-      <Hero/>
-      <BlogList/>
-      <About/>
-      <CreatePage/>
-      <BlogDetailPage title='Blog Title' body='lorem ipsum' creator='4f33...fv2' owner='4f33...fv2' onSale={true}/>
+      <Nav setPage={setPage}/>
+      {
+        page==='home' && 
+        <>
+          <Hero/>
+          <BlogList/>
+        </>
+      }
+      {
+        page==='about' && 
+        <About/>
+      }
+      {
+        page==='create' &&
+        <CreatePage/>
+      }
+      {
+        page==='details' &&
+        <BlogDetailPage title='Blog Title' body='lorem ipsum' creator='4f33...fv2' owner='4f33...fv2' onSale={true}/>
+      }
       <Footer/>
     </div>
   )
