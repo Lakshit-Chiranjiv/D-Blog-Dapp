393e65c7a665fbba4b8a05a98878cb1f66dc499a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 10:36:39 +0530

    added shadow and hover scale effect

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({btnText,txtSize}) => {
   return (
-    <button className={`bg-white rounded py-4 px-8 text-${txtSize}`}>
+    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 transition-all text-${txtSize}`}>
         {btnText}
     </button>
   )
