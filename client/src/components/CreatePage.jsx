76e4c34ac1a10106cc713fdfcf4adc8a2f4880c0
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 20:39:08 +0530

    create page component

---
+++ client/src/components/CreatePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+
+const CreatePage = () => {
+  return (
+    <section className='mx-16 my-8'>
+        <h2 className='text-3xl text-white '>Create your Blog </h2>
+        <div className="flex justify-center items-center gap-16">
+            <img src="./../assets/publish.svg" alt="publish" className='w-96' />
+            <div className="p-6">
+                <label htmlFor="name" className='text-left text-white '>Blog Title</label>
+                <input type="text" className='w-full my-2 rounded p-4 h-12 border-none mb-6'/>
+                <label htmlFor="name" className='text-left text-white '>Blog Body</label>
+                <textarea name="body" id="blogbody" cols="30" rows="10" className='my-2 w-full rounded p-4 border-none mb-6 h-48'></textarea>
+                <Button extraClasses='w-full' btnText='Publish Blog' txtSize='lg'/>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default CreatePage
