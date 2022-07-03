a457bdd54d42f6b650abcd1e06d0007e968e0228
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:31:35 +0530

    border color

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -4,7 +4,7 @@ import BlogCard from './BlogCard'
 const BlogList = () => {
   return (
     <section className='my-8 mx-16'>
-        <h2 className='text-4xl text-white mb-6 border-b-2 p-2'>Published D-Blogs</h2>
+        <h2 className='text-4xl text-white mb-6 border-b-2 border-gray-400 p-2'>Published D-Blogs</h2>
         <section className='flex gap-10 flex-wrap'>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
