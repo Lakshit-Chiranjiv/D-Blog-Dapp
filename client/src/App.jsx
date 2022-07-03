52a4ad4f14e392b1647e793aada8a5e98b910db6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 09:54:09 +0530

    nav created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,31 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <h1 className='bg-pink-400'>Hello</h1>
+    <div className="App bg-slate-600">
+      <nav className='flex justify-around items-center py-6 mx-16'>
+        <a href="#">
+          <div className="bg-black p-4 rounded cursor-pointer">
+            <h1 className="text-4xl text-white"><span className='text-6xl'>D</span> Blog</h1>
+          </div>
+        </a>
+        <div className="flex gap-10 items-center">
+          <a href="#">
+            <div className="bg-black p-2 rounded cursor-pointer">
+              <h3 className="text-xl text-white">Home</h3>
+            </div>
+          </a>
+          <a href="#">
+            <div className="bg-black p-2 rounded cursor-pointer">
+              <h3 className="text-xl text-white">Create</h3>
+            </div>
+          </a>
+          <a href="#">
+            <div className="bg-black p-2 rounded cursor-pointer">
+              <h3 className="text-xl text-white">About</h3>
+            </div>
+          </a>
+        </div>
+      </nav>
     </div>
   )
 }
