bd384eb4d23d56c81bf85b576385bdecdc131fea
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:51:44 +0530

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-white py-4 border-gray-400 mx-16 border-t-2'>
+        <span className='text-2xl'>&copy;</span> Copyright | 2022 | By Lakshit | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
