4c0c46b6f423cf5b62499fe789d54d1945cb4b0a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:58:12 +0530

    id bloglist added

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -3,7 +3,7 @@ import BlogCard from './BlogCard'
 
 const BlogList = () => {
   return (
-    <section className='my-8 mx-16 py-4'>
+    <section className='my-8 mx-16 py-4' id='bloglist'>
         <h2 className='text-4xl text-white mb-6 border-b-2 border-gray-400 p-2'>Published D-Blogs</h2>
         <section className='gap-10 flex-wrap grid lg:grid-cols-3 md:grid-cols-2'>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
