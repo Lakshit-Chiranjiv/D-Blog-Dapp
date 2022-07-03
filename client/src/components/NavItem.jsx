e562bb3b5a2b3070a83d5e70240ef91b6ed9e99d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:57:46 +0530

    link loc prop

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const NavItem = ({navItemName}) => {
+const NavItem = ({navItemName,linkLoc}) => {
   return (
-    <a href="#">
+    <a href={linkLoc}>
         <div className="bg-black p-2 rounded cursor-pointer hover:scale-105 transition-all">
             <h3 className="text-xl text-white">{navItemName}</h3>
         </div>
