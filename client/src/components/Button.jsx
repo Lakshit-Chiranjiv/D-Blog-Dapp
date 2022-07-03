6d105b9eba57b9ded9c1e0e753ad5a88a806294b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:15:13 +0530

    button effects added

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({btnText,txtSize}) => {
   return (
-    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 transition-all text-${txtSize}`}>
+    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 hover:bg-black hover:shadow-none hover:text-white transition-all text-${txtSize}`}>
         {btnText}
     </button>
   )
