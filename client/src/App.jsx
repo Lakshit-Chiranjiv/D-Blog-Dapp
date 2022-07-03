63646950a95d91d8a89e826934d19b8dcae28093
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 23:23:06 +0530

    used blog details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import About from './components/About'
+import BlogDetailPage from './components/BlogDetailPage'
 import BlogList from './components/BlogList'
 import Button from './components/Button'
 import CreatePage from './components/CreatePage'
@@ -11,12 +12,13 @@ import Nav from './components/Nav'
 function App() {
 
   return (
-    <div className="App bg-slate-600 mb-0">
+    <div className="App bg-gradient-to-tl from-slate-500 via-gray-700 to-neutral-400 mb-0">
       <Nav/>
       <Hero/>
       <BlogList/>
       <About/>
       <CreatePage/>
+      <BlogDetailPage title='Blog Title' body='lorem ipsum' creator='4f33...fv2' owner='4f33...fv2' onSale={true}/>
       <Footer/>
     </div>
   )
