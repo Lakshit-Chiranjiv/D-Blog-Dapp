fee11d8f0d3e7d800e09a3355c49c1ea63cc6f48
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:52:32 +0530

    used footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 
 import './App.css'
 import BlogList from './components/BlogList'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
 
 function App() {
 
   return (
-    <div className="App bg-slate-600">
+    <div className="App bg-slate-600 mb-0">
       <Nav/>
       <Hero/>
       <BlogList/>
+      <Footer/>
     </div>
   )
 }
