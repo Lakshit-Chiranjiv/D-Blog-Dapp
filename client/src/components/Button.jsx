74ffc6dcab03f4a4d0cd35dd915fd0544734ca59
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 23:00:11 +0530

    button bg gradient on hover

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({btnText,txtSize,extraClasses}) => {
   return (
-    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 hover:bg-black hover:shadow-none hover:text-white transition-all text-${txtSize} ${extraClasses}`}>
+    <button className={`bg-white rounded py-4 px-8 shadow-white hover:scale-95 hover:bg-gradient-to-t from-stone-900 via-violet-800 to-black hover:shadow-none hover:text-white transition-all text-${txtSize} ${extraClasses}`}>
         {btnText}
     </button>
   )
