e5da5d9229cd3b429300cb69981780ba880f9216
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:42:35 +0530

    blog list component

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -3,9 +3,11 @@ import BlogCard from './BlogCard'
 
 const BlogList = () => {
   return (
-    <section className='my-8 mx-16'>
+    <section className='my-8 mx-16 py-4'>
         <h2 className='text-4xl text-white mb-6 border-b-2 border-gray-400 p-2'>Published D-Blogs</h2>
-        <section className='flex gap-10 flex-wrap'>
+        <section className='gap-10 flex-wrap grid lg:grid-cols-3 md:grid-cols-2'>
+            <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
+            <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
