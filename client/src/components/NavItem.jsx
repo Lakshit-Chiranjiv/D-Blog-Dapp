b3641b08b38f014accee6288255db50d321cb138
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-03 12:16:18 +0530

    nav item effects added

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavItem = ({navItemName}) => {
   return (
     <a href="#">
-        <div className="bg-black p-2 rounded cursor-pointer">
+        <div className="bg-black p-2 rounded cursor-pointer hover:scale-105 transition-all">
             <h3 className="text-xl text-white">{navItemName}</h3>
         </div>
     </a>
