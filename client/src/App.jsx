8429c67b8f53220073b7c9600435541da94efc00
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-04 21:32:36 +0530

    created detail context and passed detail created states

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { createContext } from 'react'
 import { useState } from 'react'
 import './App.css'
 import About from './components/About'
@@ -9,16 +10,43 @@ import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
 
+export const DetailContext = createContext()
+
 function App() {
+  //paging
   const [page,setPage] = useState('home')
+
+  //input fields
   const [blogTitleInput,setBlogTitleInput] = useState('')
   const [blogBodyInput,setBlogBodyInput] = useState('')
   const [blogPriceInput,setBlogPriceInput] = useState(0)
   const [blogSaleInput,setBlogSaleInput] = useState(false)
 
+  //blog details page data
+  const [detailPageTitle,setDetailPageTitle] = useState('Blog Title')
+  const [detailPageBody,setDetailPageBody] = useState('Blog body lorem ipsum')
+  const [detailPagePrice,setDetailPagePrice] = useState(0)
+  const [detailPageCreator,setDetailPageCreator] = useState('scsc...3r2')
+  const [detailPageOwner,setDetailPageOwner] = useState('scs3...2cs')
+  const [detailPageReadBy,setDetailPageReadBy] = useState(0)
+  const [detailPageSale,setDetailPageSale] = useState(false)
+
+  const detailContextValue = {
+    title: detailPageTitle,
+    body: detailPageBody,
+    price: detailPagePrice,
+    creator: detailPageCreator,
+    owner: detailPageOwner,
+    readBy: detailPageReadBy,
+    onSale: detailPageSale
+  }
+
   return (
     <div className="App bg-gradient-to-tl from-slate-500 via-gray-700 to-neutral-400 mb-0">
       <Nav setPage={setPage}/>
+      <button onClick={()=>{
+        setPage('details')
+      }}>click</button>
       {
         page==='home' && 
         <>
@@ -28,7 +56,7 @@ function App() {
       }
       {
         page==='about' && 
-        <About setPage={setPage}/>
+          <About setPage={setPage}/>
       }
       {
         page==='create' &&
@@ -36,7 +64,9 @@ function App() {
       }
       {
         page==='details' &&
-        <BlogDetailPage title='Blog Title' body='lorem ipsum' creator='4f33...fv2' owner='4f33...fv2' onSale={true}/>
+        <DetailContext.Provider value={detailContextValue} >
+          <BlogDetailPage/>
+        </DetailContext.Provider>
       }
       <Footer/>
     </div>
