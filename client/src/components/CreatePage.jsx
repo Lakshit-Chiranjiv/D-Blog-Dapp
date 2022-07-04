0be3e1bb721f217a7ea67dfb9823a62bb3ebe08c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-04 11:17:59 +0530

    added checkbox for onsale input

--- client/src/components/CreatePage.jsx
+++ client/src/components/CreatePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button'
 
-const CreatePage = ({blogTitleInput,setBlogTitleInput,blogBodyInput,setBlogBodyInput}) => {
+const CreatePage = ({blogTitleInput,setBlogTitleInput,blogBodyInput,setBlogBodyInput,blogSaleInput,setBlogSaleInput}) => {
   return (
     <section className='mx-16 my-8'>
         <h2 className='text-3xl text-white '>Create your Blog </h2>
@@ -12,6 +12,10 @@ const CreatePage = ({blogTitleInput,setBlogTitleInput,blogBodyInput,setBlogBodyI
                 <input type="text" className='w-full my-2 rounded p-4 h-12 border-none mb-6' value={blogTitleInput} onChange={(e) => {setBlogTitleInput(e.target.value)}}/>
                 <label htmlFor="name" className='text-left text-white '>Blog Body</label>
                 <textarea name="body" id="blogbody" cols="30" rows="10" className='my-2 w-full rounded p-4 border-none mb-6 h-48' value={blogBodyInput} onChange={(e) => {setBlogBodyInput(e.target.value)}}></textarea>
+                <div className="flex justify-center items-center gap-2 mb-6">
+                    <input type="checkbox" name="onsale" id="onsale" className='h-6 w-8' checked={blogSaleInput} onChange={(e)=>{setBlogSaleInput(e.target.checked)}}/>
+                    <label htmlFor="onsale" className='text-white'>On sale</label>
+                </div>
                 <Button extraClasses='w-full' btnText='Publish Blog' txtSize='lg'/>
             </div>
         </div>
