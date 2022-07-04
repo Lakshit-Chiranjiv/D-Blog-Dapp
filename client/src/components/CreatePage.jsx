71db373d477e15d553ed3a221f66413340162d06
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-04 10:53:05 +0530

    wired title and body input states

--- client/src/components/CreatePage.jsx
+++ client/src/components/CreatePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button'
 
-const CreatePage = () => {
+const CreatePage = ({blogTitleInput,setBlogTitleInput,blogBodyInput,setBlogBodyInput}) => {
   return (
     <section className='mx-16 my-8'>
         <h2 className='text-3xl text-white '>Create your Blog </h2>
@@ -9,9 +9,9 @@ const CreatePage = () => {
             <img src="./../assets/publish.svg" alt="publish" className='w-96' />
             <div className="p-6">
                 <label htmlFor="name" className='text-left text-white '>Blog Title</label>
-                <input type="text" className='w-full my-2 rounded p-4 h-12 border-none mb-6'/>
+                <input type="text" className='w-full my-2 rounded p-4 h-12 border-none mb-6' value={blogTitleInput} onChange={(e) => {setBlogTitleInput(e.target.value)}}/>
                 <label htmlFor="name" className='text-left text-white '>Blog Body</label>
-                <textarea name="body" id="blogbody" cols="30" rows="10" className='my-2 w-full rounded p-4 border-none mb-6 h-48'></textarea>
+                <textarea name="body" id="blogbody" cols="30" rows="10" className='my-2 w-full rounded p-4 border-none mb-6 h-48' value={blogBodyInput} onChange={(e) => {setBlogBodyInput(e.target.value)}}></textarea>
                 <Button extraClasses='w-full' btnText='Publish Blog' txtSize='lg'/>
             </div>
         </div>
