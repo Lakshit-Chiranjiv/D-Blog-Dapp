99995c076d8024c75270035c86aa9f4d5d7380c3
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-05 16:12:15 +0530

    commented dummy blogs

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -8,11 +8,11 @@ const BlogList = ({allBlogs}) => {
     <section className='my-8 mx-16 py-4' id='bloglist'>
         <h2 className='text-4xl text-white mb-6 border-b-2 border-gray-400 p-2'>Published D-Blogs</h2>
         <section className='gap-10 flex-wrap grid lg:grid-cols-3 md:grid-cols-2'>
+            {/* <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
-            <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
-            <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/>
+            <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/> */}
             {
               allBlogs.map((blog,i) => (
                 <BlogCard title={blog.title} snip={snipetizeBody(blog.body)} readBy={blog.readBy} ownerShort={addressReducer(blog.owner)} key={i}/>
