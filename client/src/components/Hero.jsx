6a16ba74798f11462ac42c762ba51a00e2754135
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-05 09:00:03 +0530

    wired connect func and connect msgs display conditionally

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Button from './Button'
 
-const Hero = () => {
+const Hero = ({account,connectMsg,connectWallet}) => {
   return (
     <section className='flex gap-12 justify-around items-center py-6 mb-8 mx-16'>
         <img src="../assets/blog.svg" alt="" className='w-96'/>
         <div>
-            <a href="#">
-                <Button btnText='Connect With Metamask' txtSize='xl'/>
-            </a>
-            <p className='text-sm text-red-600 bg-white p-4 rounded mt-4'>Some error occured</p>
-            <p className='text-sm text-green-500 bg-white p-4 rounded mt-4'>Connected <br /> 0x279ef018cffdd199d42c68e2df2911f3bb94040f</p>
+            {
+              !account && 
+              <a href="#" onClick={connectWallet}>
+                  <Button btnText='Connect With Metamask' txtSize='xl'/>
+              </a>  
+            }
+            {
+              account &&
+              <p className='text-sm text-green-500 bg-white p-4 rounded mt-4'>Connected <br /> {account}</p>
+            }
+            {
+              connectMsg && 
+              <p className='text-sm text-red-600 bg-white p-4 rounded mt-4'>Some error occured <br /> {connectMsg}</p>
+            }
         </div>
     </section>
   )
