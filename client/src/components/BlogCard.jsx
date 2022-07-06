d6a06982c31e71d03a98e9ccdd2304dee034228f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-06 20:26:46 +0530

    added onclick handler as readhandler on readbtn

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BlogCard = ({title,snip,readBy,ownerShort}) => {
+const BlogCard = ({bid,title,snip,readBy,ownerShort,readBlogHandler}) => {
   return (
     <div className='bg-white rounded-lg shadow-2xl py-4 px-6 flex-1 text-left hover:scale-95 transition-all'>
         <h3 className="text-2xl mb-2">{title}</h3>
@@ -10,7 +10,7 @@ const BlogCard = ({title,snip,readBy,ownerShort}) => {
                 <p className='text-sm'>read by : <span className='text-purple-500 text-md'>{Number(readBy.toString())}</span></p>
                 <p className='text-sm'>Owner : <span className='text-indigo-500 text-md'>{ownerShort}</span></p>
             </div>
-            <button className='bg-purple-300 p-2 px-4 rounded hover:scale-95 hover:bg-purple-500 hover:text-white transition-all'>Read</button>
+            <button className='bg-purple-300 p-2 px-4 rounded hover:scale-95 hover:bg-purple-500 hover:text-white transition-all' onClick={()=>{readBlogHandler(bid)}}>Read</button>
         </div>
     </div>
   )
