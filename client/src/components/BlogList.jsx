3b652a058549ffe70f717d5f70e6ceab8bd7525d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-06 20:26:25 +0530

    passed readhandler and bid prop to blogcard

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -3,7 +3,7 @@ import BlogCard from './BlogCard'
 import { snipetizeBody } from './../util/snipetizeBody.js'
 import { addressReducer } from './../util/addressReducer.js'
 
-const BlogList = ({allBlogs}) => {
+const BlogList = ({allBlogs,readBlogHandler}) => {
   return (
     <section className='my-8 mx-16 py-4' id='bloglist'>
         <h2 className='text-4xl text-white mb-6 border-b-2 border-gray-400 p-2'>Published D-Blogs</h2>
@@ -15,7 +15,7 @@ const BlogList = ({allBlogs}) => {
             <BlogCard title='Blog title' snip='blog snippet' readBy={342} ownerShort='rrcs...4f3'/> */}
             {
               allBlogs.map((blog,i) => (
-                <BlogCard title={blog.title} snip={snipetizeBody(blog.body)} readBy={blog.readBy} ownerShort={addressReducer(blog.owner)} key={i}/>
+                <BlogCard bid={blog.id} title={blog.title} snip={snipetizeBody(blog.body)} readBy={blog.readBy} ownerShort={addressReducer(blog.owner)} key={i} readBlogHandler={readBlogHandler}/>
               ))
             }
         </section>
