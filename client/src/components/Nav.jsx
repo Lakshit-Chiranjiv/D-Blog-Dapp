1fecc051b65c6de109b5fabab6a298d42650b633
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-22 17:25:50 +0530

    added navigate funcs in nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import NavItem from './NavItem'
 
 const Nav = ({}) => {
+
+  const navigate = useNavigate();
+
   return (
     <nav className='flex justify-around items-center py-6 mb-8 mx-16'>
-        <a href="#" onClick={()=>{}}>
+        <a href="#" onClick={()=>{navigate('/')}}>
             <div className="bg-black p-4 rounded cursor-pointer">
                 <h1 className="text-4xl text-white"><span className='text-6xl'>D</span> Blog</h1>
             </div>
         </a>
         <div className="flex gap-10 items-center">
-            <NavItem navItemName='Read' linkLoc='#bloglist' navFunc={()=>{}}/>
-            <NavItem navItemName='Create' navFunc={()=>{}}/>
-            <NavItem navItemName='About' navFunc={()=>{}}/>
+            <NavItem navItemName='Read' linkLoc='#bloglist' navFunc={()=>{navigate('/')}}/>
+            <NavItem navItemName='Create' navFunc={()=>{navigate('/create')}}/>
+            <NavItem navItemName='About' navFunc={()=>{navigate('/about')}}/>
         </div>
     </nav>
   )
