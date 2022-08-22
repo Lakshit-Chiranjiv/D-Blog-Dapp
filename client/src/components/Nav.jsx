a0f73c934b9d6f656b2d2aee66cc2164c6eac22f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-22 17:20:20 +0530

    removed setPage props and calls

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import NavItem from './NavItem'
 
-const Nav = ({setPage}) => {
+const Nav = ({}) => {
   return (
     <nav className='flex justify-around items-center py-6 mb-8 mx-16'>
-        <a href="#" onClick={()=>{setPage('home')}}>
+        <a href="#" onClick={()=>{}}>
             <div className="bg-black p-4 rounded cursor-pointer">
                 <h1 className="text-4xl text-white"><span className='text-6xl'>D</span> Blog</h1>
             </div>
         </a>
         <div className="flex gap-10 items-center">
-            <NavItem navItemName='Read' linkLoc='#bloglist' navFunc={()=>{setPage('home')}}/>
-            <NavItem navItemName='Create' navFunc={()=>{setPage('create')}}/>
-            <NavItem navItemName='About' navFunc={()=>{setPage('about')}}/>
+            <NavItem navItemName='Read' linkLoc='#bloglist' navFunc={()=>{}}/>
+            <NavItem navItemName='Create' navFunc={()=>{}}/>
+            <NavItem navItemName='About' navFunc={()=>{}}/>
         </div>
     </nav>
   )
