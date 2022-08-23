45cbaeaf323d662b55f0c17813329c974484d65b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-23 19:44:47 +0530

    not found page

---
+++ client/src/components/NotFoundPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => {
+  return (
+    <div className='h-screen mx-12'>
+        <h2 className='text-5xl my-4 mb-6 text-white'>404 Not Found</h2>
+        <p className='text-gray-300'>Seems you are Lost</p>
+
+        <div className="flex justify-center items-center p-4 my-6">
+            <img src="./../../assets/lost.svg" alt="lost" className='w-96' />
+        </div>
+
+        <Link to='/'>
+          <button className="bg-purple-500 p-4 w-full rounded-lg my-6 hover:scale-95 transition-all">Head Over to the Home</button>
+        </Link>
+    </div>
+  )
+}
+
+export default NotFoundPage
