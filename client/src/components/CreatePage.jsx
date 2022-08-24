66f3d5b0dd8096ed567f4285ede32cea55fddf81
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-24 19:58:20 +0530

    changed min blog value to 2

--- client/src/components/CreatePage.jsx
+++ client/src/components/CreatePage.jsx
@@ -15,7 +15,7 @@ const CreatePage = ({blogCreationInputs,handleBlogCreationInput,createBlogHandle
                 <textarea name="blogBodyInput" id="blogbody" cols="30" rows="10" className='my-2 w-full rounded p-4 border-none mb-6 h-48' value={blogCreationInputs.blogBodyInput} onChange={handleBlogCreationInput} placeholder='Enter blog body'></textarea>
 
                 <label htmlFor="price" className='text-left text-white '>Blog Price</label>
-                <input type="number" name='blogPriceInput' className='w-full my-2 rounded p-4 h-12 border-none' value={blogCreationInputs.blogPriceInput} onChange={handleBlogCreationInput}/>
+                <input type="number" min={2} name='blogPriceInput' className='w-full my-2 rounded p-4 h-12 border-none' value={blogCreationInputs.blogPriceInput} onChange={handleBlogCreationInput}/>
                 <p className='text-red-400 mb-6 text-[12px]'>Note : Entered value will be multiplied by 0.001 eth</p>
 
                 <div className="flex justify-center items-center gap-2 mb-6">
