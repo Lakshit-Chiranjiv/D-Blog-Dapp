ba09be4cb2f363376877a3e4468d852603e72e2f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-24 23:18:48 +0530

    called getAllBlogs on logo click

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import NavItem from './NavItem'
 
-const Nav = ({account}) => {
+const Nav = ({account,getAllBlogs}) => {
 
   const navigate = useNavigate();
 
   return (
     <nav className='flex justify-around items-center py-6 mb-8 mx-16'>
-        <a href="#" onClick={()=>{navigate('/')}}>
+        <a href="#" onClick={()=>{
+          getAllBlogs();
+          navigate('/')
+        }}>
             <div className="bg-black p-4 rounded cursor-pointer">
                 <h1 className="text-4xl text-white"><span className='text-6xl'>D</span> Blog</h1>
             </div>
