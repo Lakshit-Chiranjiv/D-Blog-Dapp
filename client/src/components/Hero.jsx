1e1f5b0df7825ae68ac5aa5a918740056b236067
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-08-25 23:45:09 +0530

    connectLoader wired

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import { addressReducer } from '../util/addressReducer'
 import Button from './Button'
+import Loader from './Loader'
 
-const Hero = ({account,connectMsg,connectWallet}) => {
+const Hero = ({account,connectMsg,connectWallet,connectWalletLoader}) => {
 
   return (
     <section className='grid grid-cols-1 md:grid-cols-2 items-center gap-12 py-6 mb-8 mx-16'>
         <img src="../assets/blog.svg" alt="" className='w-96'/>
         <div>
-            {
-              !account && 
-              <a href="#" onClick={connectWallet}>
-                  <Button btnText='Connect With Metamask' txtSize='xl'/>
-              </a>  
-            }
-            {
-              account &&
-              <p className='text-sm text-green-500 bg-white p-4 rounded mt-4'>Connected <br /> {((window.innerWidth < 1025 && window.innerWidth > 767) || (window.innerWidth < 565))?addressReducer(account):account}</p>
-            }
-            {
-              connectMsg && 
-              <p className='text-sm text-red-600 bg-white p-4 rounded mt-4'>Some error occured <br /> {connectMsg}</p>
-            }
+          {
+            connectWalletLoader ? 
+            <Loader/>:
+            <>
+              {
+                !account && 
+                <a href="#" onClick={connectWallet}>
+                    <Button btnText='Connect With Metamask' txtSize='xl'/>
+                </a>  
+              }
+              {
+                account &&
+                <p className='text-sm text-green-500 bg-white p-4 rounded mt-4'>Connected <br /> {((window.innerWidth < 1025 && window.innerWidth > 767) || (window.innerWidth < 565))?addressReducer(account):account}</p>
+              }
+              {
+                connectMsg && 
+                <p className='text-sm text-red-600 bg-white p-4 rounded mt-4'>Some error occured <br /> {connectMsg}</p>
+              }
+            </>
+          }
         </div>
     </section>
   )
